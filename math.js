3f17f6d9b53cbfe3b5ced2e89864fa19ffd97a43
Prathoseraaj V <prathoseraaj0312@gmail.com>
2025-01-27 16:09:13 +0530

    feat: another method

--- math.js
+++ math.js
@@ -1,6 +1,7 @@
-const add = (a,b) => a+b;
-const sub = (a,b) => a-b;
-const mul = (a,b) => a*b;
-const div = (a,b) => a/b;
+exports.add = (a,b) => a+b;
+exports.sub= (a,b) => a-b;
+exports.mul = (a,b) => a*b;
+exports.div = (a,b) => a/b;
 
-module.exports= {add, sub, mul, div};
+//module.exports= {add, sub, mul, div}; 
+// we can also derectly export it in the same line
