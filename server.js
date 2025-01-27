de220e5c378092c34c63687e8a58c58b5875c9d9
Prathoseraaj V <prathoseraaj0312@gmail.com>
2025-01-27 16:06:52 +0530

    feat: method for importing

--- server.js
+++ server.js
@@ -15,3 +15,21 @@ console.log(path.basename(__filename));
 console.log(path.extname(__filename));
 
 console.log(path.parse(__filename)); //parse the every detail of the file 
+
+const math = require('./math'); 
+const {add,sub,mul,div} = require('./math'); 
+
+// from 19 line
+console.log(math.add(3,3));
+console.log(math.sub(4,4));
+console.log(math.mul(12,5));
+console.log(math.div(12,6));
+
+console.log('________________') // to seprate
+
+// we can also destucture it and execute 
+//form 20 line
+console.log(add(3,3));
+console.log(sub(4,4));
+console.log(mul(12,5));
+console.log(div(12,6));
