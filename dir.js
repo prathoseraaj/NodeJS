bf8452f9d0fd34c59fbee2fe98997dadcc3bbf58
Prathoseraaj V <prathoseraaj0312@gmail.com>
2025-01-27 23:32:05 +0530

    feat: fs

--- dir.js
+++ dir.js
@@ -1,9 +1,15 @@
 const fs = require('fs')
 
-fs.mkdir('./new',(err)=>{
-    if(err) throw err
-    console.log('Directory created');
-});
+if(!fs.existsSync('./new')){
+    fs.mkdir('./new',(err)=>{
+        if(err) throw err
+        console.log('Directory created');
+    });
+}
+else{
+    console.log('file already exists')
+}
+
 
 //if the file has error we should use process from node
 process.on('uncaughtException', err => {
