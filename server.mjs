89a924f6098c4a57e011150fbee2fbfe3a8e7b04
Prathoseraaj V <prathoseraaj0312@gmail.com>
2025-01-27 21:43:17 +0530

    feat: fs

--- server.mjs
+++ server.mjs
@@ -1,6 +1,8 @@
 import { readFile } from 'node:fs';
 
-readFile('./files/start.txt', (err, data) => {
+readFile('./files/start.txt', 'utf8', (err, data) => {
   if (err) throw err;
-  console.log(data);
+  //console.log(data.toString()); //convert to string
+  console.log(data); //as we use encoding 
+  //console.log(data);
 }); 
