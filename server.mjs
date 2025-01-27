db99844a31f66a8dcb5780272733acdb2586de16
Prathoseraaj V <prathoseraaj0312@gmail.com>
2025-01-27 21:54:40 +0530

    feat: fs

--- server.mjs
+++ server.mjs
@@ -1,6 +1,7 @@
 import { readFile } from 'node:fs';
+import path from 'node:path';
 
-readFile('./files/start.txt', 'utf8', (err, data) => {
+readFile(path.join(__dirname,'files','start.txt'), 'utf8', (err, data) => {
   if (err) throw err;
   //console.log(data.toString()); //convert to string
   console.log(data); //as we use encoding 
