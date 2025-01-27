c1033bb3113f54224423f3c670429e105530074c
Prathoseraaj V <prathoseraaj0312@gmail.com>
2025-01-27 15:54:15 +0530

    feat: parse

--- server.js
+++ server.js
@@ -14,3 +14,4 @@ console.log(path.dirname(__filename));
 console.log(path.basename(__filename));
 console.log(path.extname(__filename));
 
+console.log(path.parse(__filename)); //parse the every detail of the file 
