12ba63b1a437404056527f1e7f55942b7f008531
Prathoseraaj V <prathoseraaj0312@gmail.com>
2025-01-28 14:08:55 +0530

    feat: fs

--- dir.js
+++ dir.js
@@ -1,7 +1,19 @@
 const fs = require('fs')
 
-if(!fs.existsSync('./new')){
+// CREATE A NEW DIRECTORY 
+
+/* if(!fs.existsSync('./new')){
     fs.mkdir('./new',(err)=>{
+        if(err) throw err
+        console.log('Directory created');
+    });
+}
+else{
+    console.log('file already exists')
+} */
+
+if(fs.existsSync('./new')){
+    fs.rmdir('./new',(err)=>{
         if(err) throw err
         console.log('Directory created');
     });
