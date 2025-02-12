8a14bbbb85390d0c76f8322d973f62171ca7760a
Prathoseraaj V <prathoseraaj0312@gmail.com>
2025-02-13 00:10:26 +0530

    fix: css

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const http = require('http');
 
-const server = http.createServer((req,res)=>{
-    res.writeHead(200,{'content-type':'text/html'});
-    res.end('<h1> Welcome to World of Leo Das Mamae </h1>')
+const server = http.createServer((req, res) => {
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.end('<h1 style="text-align: center;">Welcome to World of Leo Das Mamae</h1>');
 });
 
-server.listen(3002, () => console.log('server is runnning in http://localhost:3002'))
+server.listen(3003, () => console.log('Server is running at http://localhost:3003'));
