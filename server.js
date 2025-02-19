b61301eaf01eb0ec5c33fa1377da483da11db761
Prathoseraaj V <prathoseraaj0312@gmail.com>
2025-02-19 22:36:51 +0530

    feat: basic structue of backend

---
+++ server.js
@@ -0,0 +1,13 @@
+//fetch and play with api
+const http = require('http');
+const express = require('express');
+const axios = require('axios');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+
+
+const url = 'https://jsonplaceholder.typicode.com/';
+
+
