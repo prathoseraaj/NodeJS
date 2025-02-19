e81265017e8fc846baa058dc2f0a294e6f29f5dd
Prathoseraaj V <prathoseraaj0312@gmail.com>
2025-02-19 22:49:07 +0530

    feat: to fetch

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 //fetch and play with api
+
 const http = require('http');
 const express = require('express');
 const axios = require('axios');
@@ -6,8 +7,17 @@ const axios = require('axios');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+const fetchdata = async(url) => {
+    try{
+        const response = await axios.get(`https://jsonplaceholder.typicode.com${url}`);
+        return response.data;
+    }
+    catch(error){
+        throw new Error('Failed to fetch data');
+    }
+};
 
 
-const url = 'https://jsonplaceholder.typicode.com/';
+
 
 
